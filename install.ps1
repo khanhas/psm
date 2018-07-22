9dd3c3c7361a120d8d781d9381aa15e67d3fd8e6
khanhas <xuankhanh963@gmail.com>
2018-07-22 21:28:28 +0700

    Fixed powershell process path and enviroment variable location

--- install.ps1
+++ install.ps1
@@ -2,17 +2,32 @@ $installPath = $MyInvocation.MyCommand.Definition
 $psmRoot = Split-Path -parent $installPath
 $psmBin = Join-Path $psmRoot "bin"
 
-if (IsWindows) {
+if ($Host.Version.Major -lt 6) {
+    Write-Output "Requires Powershell version >= 6."
+    Write-Output "Exit."
+    return
+}
+
+$envKey = "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment\"
+
+if ($IsWindows) {
     If (-NOT ([Security.Principal.WindowsPrincipal][Security.Principal.WindowsIdentity]::GetCurrent()).IsInRole([Security.Principal.WindowsBuiltInRole] "Administrator")) {
-        $arguments = "& '" + $installPath + "'"
+        $arguments = "-File " + $installPath
+        $shellPath = (Get-Process -Id $PID).Name
         # Execute this script again but elevated
-        Start-Process powershell -Verb runAs -ArgumentList $arguments
+        Start-Process $shellPath -Verb runAs -ArgumentList $arguments
     } else {
+        $path = [Microsoft.Win32.Registry]::GetValue($envKey, "PATH", "")
+        if ($path -like "*$($psmBin)*") {
+            break
+        }
         [Microsoft.Win32.Registry]::SetValue(
-            "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Session Manager\Environment\",
-            "PSMROOT",
-            $psmBin,
+            $envKey,
+            "PATH",
+            "$($path);$($psmBin)",
             [Microsoft.Win32.RegistryValueKind]::ExpandString
         )
     }
 }
+
+Write-Output "Succeed!"
